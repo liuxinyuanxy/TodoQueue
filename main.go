0a4037a9be70500abb0e28c819a95df8fc46d72a
T_horn <470898635@qq.com>
2022-06-23 12:19:04 +0800

    merge

--- main.go
+++ main.go
@@ -11,7 +11,22 @@ import (
 	"TodoQueue/model"
 )
 
+type LogFormatter struct{}
+
+func (slf *LogFormatter) Format(entry *logrus.Entry) ([]byte, error) {
+	timestamp := time.Now().Local().Format("06/01/02 15:04:05")
+	msg := fmt.Sprintf("[%s] %s (%s:%d): %s\n", entry.Level,
+		timestamp,
+		filepath.Base(entry.Caller.File),
+		entry.Caller.Line,
+		entry.Message)
+	return []byte(msg), nil
+}
+
 func main() {
+	logrus.SetReportCaller(true)
+	logrus.SetFormatter(&LogFormatter{})
+
 	model.Init()
 	app.InitWebFramework()
 	app.StartServer()
