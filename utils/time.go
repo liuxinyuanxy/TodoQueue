c7f6b2a1a796a896a1bbdc17ad118746182a8814
T_horn <470898635@qq.com>
2022-07-01 22:37:02 +0800

    change minite to second

--- utils/time.go
+++ utils/time.go
@@ -13,7 +13,7 @@ func CurrentTime() string {
 func TimeDuration(startT, endT string) uint {
 	sT, _ := time.Parse("2006-01-02 15:04:05", startT)
 	eT, _ := time.Parse("2006-01-02 15:04:05", endT)
-	durT := eT.Sub(sT).Minutes()
+	durT := eT.Sub(sT).Seconds()
 	logrus.Infof("from: %v to: %v  dur:%v", sT, eT, durT)
 	return uint(durT)
 }
