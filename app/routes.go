d9ce95a1b03af8b4a2530bbe40cd0a9560bc4add
T_horn <470898635@qq.com>
2022-07-01 15:49:17 +0800

    update cors

--- app/routes.go
+++ app/routes.go
@@ -18,9 +18,10 @@ func ping(c echo.Context) error {
 func addRoutes() {
 
 	e.Use(echoMiddleware.CORSWithConfig(echoMiddleware.CORSConfig{
-		AllowOrigins:     []string{"http://124.221.92.18:9404", "http://localhost:9404"},
+		AllowOrigins:     []string{"http://124.221.92.18:80"},
 		AllowCredentials: true,
 	}))
+	//e.Use(echoMiddleware.BodyDumpWithConfig(DefaultBodyDumpConfig))
 	api := e.Group("api")
 
 	api.GET("/doc/*", echoSwagger.WrapHandler)
@@ -67,3 +68,20 @@ func addRoutes() {
 
 	}
 }
+
+//var DefaultBodyDumpConfig = echoMiddleware.BodyDumpConfig{
+//	Skipper: BodyDumpDefaultSkipper,
+//	Handler: func(c echo.Context, reqBody []byte, resBody []byte) {
+//		println("API请求结果拦截：", string(resBody), c.RealIP(), string(reqBody))
+//		// 1、解析返回的json数据，判断接口执行成功或失败。如： {"code":"200","data":"test","msg":"请求成功"}
+//		// 2、保存操作日志
+//	},
+//}
+//
+//// 排除文件，如果您的请求/响应有效负载非常大，例如文件上载/下载，需要进行排查。否则将影响响应时间
+//func BodyDumpDefaultSkipper(c echo.Context) bool {
+//	if strings.Contains(c.Path(), "/api/files/") {
+//		return true
+//	}
+//	return false
+//}
